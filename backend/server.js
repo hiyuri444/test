ec7af553d7d752abee7435ec0c2ed1fc3b97f391
hyur <hiyuri444@gmail.com>
2026-04-21 20:37:23 +0800

    a

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,12 @@
 const express = require("express");
 const app = express();
+const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 
 app.get("/api", (req, res) => {
   res.send("Backend is running 🚀");
 });
-
-const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
