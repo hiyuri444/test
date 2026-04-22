56f4dd36f57e2ec633c00297cb05a10831382e81
hyur <hiyuri444@gmail.com>
2026-04-22 16:26:58 +0800

    first live backend test

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,64 @@
 const express = require("express");
+const cors = require("cors");
+const pool = require("./db");
 const app = express();
-const PORT = process.env.PORT || 3000;
+const port = process.env.PORT || 3000;
 
+let timer = 2
+
+app.use(cors());
 app.use(express.json());
 
-app.get("/api", (req, res) => {
-  res.send("Backend is running 🚀");
+app.post("/board", (req, res) => {
+
+  if (timer >= 0) {
+    res.status(400).json({ status: "error", message: "Timer has not expired" });
+    return;
+  }
+
+  timer = 120
+  const { message, author, x, y, angle } = req.body;
+
+  try {
+    pool.query("INSERT INTO board (message, author, x, y, angle) VALUES ($1, $2, $3, $4, $5)", [message, author, x, y, angle], (err, result) => {
+      if (err) {
+        console.error("Error executing query", err.stack);
+        res.status(500).json({ status: "error", message: "Database query failed" });
+      } else {
+        console.log(`Received message: ${message} from author: ${author}`);
+        res.json({ status: "success", message: "Note received" });
+      }
+    });
+  } catch (error) {
+    console.error("Error executing query", error.stack);
+    res.status(500).json({ status: "error", message: "Database query failed" });
+  }
 });
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+app.get("/board", (req, res) => {
+  try {
+    pool.query("SELECT * FROM board", (err, result) => {
+      if (err) {
+        console.error("Error executing query", err.stack);
+        res.status(500).json({ status: "error", message: "Database query failed" });
+      } else {
+        res.json(result.rows);
+      }
+    });
+  } catch (error) {
+    console.error("Error executing query", error.stack);
+    res.status(500).json({ status: "error", message: "Database query failed" });
+  }
 });
+
+app.get("/timer", (req, res) => {
+  res.json({ timer: timer });
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
+
+setInterval(() => {
+  timer = timer - 1
+}, 1000);
--- frontend/script.js
+++ frontend/script.js
@@ -6,13 +6,38 @@ const authortextarea = document.getElementById("authortextarea")
 let activenote
 let offset
 let notewidth
-let timer = 2
+let timer = fetch("https://test-xajq.onrender.com/timer", {
+    method: "GET",
+    headers: {
+        "Content-Type": "application/json"
+    }})
+    .then(res => res.json())
+    .then(data => {
+        timer = data.timer;
+        updatetimerdisplay();
+    });
 
 function formattime(seconds) {
   const mins = Math.floor(seconds / 60);
   const secs = seconds % 60;
 
   return mins + ":" + secs.toString().padStart(2, "0");
+}
+
+function getRotationDegrees(element) {
+  const style = window.getComputedStyle(element);
+  const transform = style.transform;
+
+  if (transform === "none") return 0;
+
+  const values = transform.match(/matrix\((.+)\)/)[1].split(", ");
+
+  const a = parseFloat(values[0]);
+  const b = parseFloat(values[1]);
+
+  const angle = Math.round(Math.atan2(b, a) * (180 / Math.PI));
+
+  return angle;
 }
 
 function createactivenote() {
@@ -81,15 +106,65 @@ board.addEventListener("click", (event) => {
             activenote.remove()
             activenote = null
         } else {
-            activenote = null
-            timer = 120
-            updatetimerdisplay()
+
+            fetch("https://test-xajq.onrender.com/board", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({
+                    message: notetextarea.value,
+                    author: authortextarea.value,
+                    x: Math.floor(event.pageX - offset.left - 60),
+                    y: Math.floor(event.pageY - offset.top + 10),
+                    angle: Math.floor(getRotationDegrees(activenote))
+                })
+            })
+            .then(res => res.json())
+            .then(data => {
+                if (data.status === "error") {
+                    location.reload()
+                } else {
+                    activenote = null
+                    timer = 120
+                    updatetimerdisplay()
+                }
+            });
         }
     }
 });
 
 notetextarea.addEventListener("input", santizeinput);
 authortextarea.addEventListener("input", santizeinput)
+
+fetch("https://test-xajq.onrender.com/board", {
+    method: "GET",
+    headers: {
+        "Content-Type": "application/json"
+    }
+}).then(res => res.json())
+.then(data => {
+    for (const datum in data) {
+
+        note = document.createElement("div");
+        note.classList.add("note");
+
+        note.style.top = data[datum].y + "px";
+        note.style.left = data[datum].x + "px";
+        note.style.transform = "rotate(" + data[datum].angle + "deg)";
+        board.appendChild(note);
+
+        notetext = document.createElement("div");
+        notetext.classList.add("notetext");
+        notetext.textContent = data[datum].message;
+        note.appendChild(notetext)
+
+        authortext = document.createElement("div");
+        authortext.classList.add("authortext");
+        authortext.textContent = "- " + data[datum].author;
+        note.appendChild(authortext)
+    }
+});
 
 setInterval(() => {
     timer = timer - 1
